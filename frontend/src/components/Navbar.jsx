e843fa2ae919b90a1c76544af776acf97dd881cd
charlybgood__x <bonavita.carlos@gmail.com>
2025-08-12 09:43:44 -0300

    edit navbar to represent sinapsialab visual styles

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-800 text-white p-4 fixed w-full flex justify-between items-center z-50">
-      <p className=''>Landing Flyer</p>
+    <nav className="bg-gray-800 text-white p-4 fixed w-full flex justify-between px-6 items-center z-50">
       <a
         href="https://sinapsialab.com"
         target='_blank'
-        title='SinapsiaLab Web'>SinapsiaLab</a>
+        title='SinapsiaLab Web'>
+        <img src="https://res.cloudinary.com/dr8pwzxzn/image/upload/v1753991068/creativesinapsistesterReal_qbe1zr.png" alt="SinapsiaLab brain" className='w-15 h-auto' />
+      </a>
+      <a href="https://sinapsialab.com" target='_blank' title='SinapsiaLab web'>
+        <img src="https://res.cloudinary.com/dr8pwzxzn/image/upload/v1751256447/SinapsiaLabThinn8_lehxp5.png" alt="SinapsiaLab svg" className='w-35 h-auto' />
+      </a>
+      <p className=''>Landing Flyer</p>
     </nav>
   )
 }
