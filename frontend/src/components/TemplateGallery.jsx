da53f3ef73daee9ac381397d2121ab486ad6c718
charlybgood__x <bonavita.carlos@gmail.com>
2025-09-04 11:40:42 -0300

    add close button and styles on selected template so user is able to change template

--- frontend/src/components/TemplateGallery.jsx
+++ frontend/src/components/TemplateGallery.jsx
@@ -1,7 +1,6 @@
-import { Eye } from 'lucide-react';
+import { Eye, X } from 'lucide-react';
 
-
-const TemplateGallery = ({ templates, onTemplateClick, selectedTemplateId }) => {
+const TemplateGallery = ({ templates, onTemplateClick, selectedTemplateId, changeSelected }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full mb-4">
       {templates.map((template) => {
@@ -9,38 +8,44 @@ const TemplateGallery = ({ templates, onTemplateClick, selectedTemplateId }) => 
         return (
           <div
             key={template.id}
-            className={`group relative rounded-2xl shadow-lg overflow-hidden hover:shadow-2xl transition-all duration-500 hover:-translate-y-2 cursor-pointer ${
-              isSelected 
-                ? 'bg-blue-50 border-2 border-blue-500 shadow-blue-200' 
-                : 'bg-white/80'
-            }`}
+            className={`group relative rounded-2xl shadow-lg overflow-hidden hover:shadow-2xl transition-all duration-500 hover:-translate-y-2 cursor-pointer ${isSelected
+              ? 'bg-blue-50 border-2 border-blue-500 shadow-blue-200'
+              : 'bg-white/80'
+              }`}
             onClick={() => onTemplateClick(template)}
           >
             <div className="relative h-48 overflow-hidden">
-            <img
-              src={template.thumbnail}
-              alt={template.name}
-              className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
-            />
-            <div className={`absolute inset-0 bg-linear-to-br ${template.color} opacity-20 group-hover:opacity-30 transition-opacity duration-300`} />
-            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <div className="bg-portfolio-text/90 backdrop-blur-xs rounded-full p-4 transform scale-75 group-hover:scale-100 transition-transform duration-300">
-                <Eye className="w-8 h-8 text-gray-700" />
+              <img
+                src={template.thumbnail}
+                alt={template.name}
+                className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+              />
+              <div className={`absolute inset-0 bg-linear-to-br ${template.color} opacity-20 group-hover:opacity-30 transition-opacity duration-300`} />
+              <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <div className="bg-portfolio-text/90 backdrop-blur-xs rounded-full p-4 transform scale-75 group-hover:scale-100 transition-transform duration-300">
+                  <Eye className="w-8 h-8 text-portfolio-accent" />
+                </div>
               </div>
+              {
+                isSelected ? <button
+                  onClick={changeSelected}
+                  className="backdrop-blur-xs rounded-full absolute bg-portfolio-text/60 text-portfolio-accent top-0 right-0 p-2 transform scale-75 flex">
+                  <X className='text-2xl text-bolder' />
+                </button> : null
+              }
             </div>
-          </div>
-          <div className="p-6">
-            <div className={`inline-block px-3 py-1 rounded-full text-xs font-semibold text-portfolio-text bg-linear-to-r ${template.color} mb-3`}>
-              Vista previa
+            <div className="p-6">
+              <div className={`inline-block px-3 py-1 rounded-full text-xs font-semibold text-portfolio-text bg-linear-to-r ${template.color} mb-3`}>
+                Vista previa
+              </div>
+              <h3 className="text-xl font-bold text-gray-800 mb-2 group-hover:text-gray-900 transition-colors">
+                {template.name}
+              </h3>
+              <p className="text-gray-600 group-hover:text-gray-700 transition-colors">
+                {template.description}
+              </p>
             </div>
-            <h3 className="text-xl font-bold text-gray-800 mb-2 group-hover:text-gray-900 transition-colors">
-              {template.name}
-            </h3>
-            <p className="text-gray-600 group-hover:text-gray-700 transition-colors">
-              {template.description}
-            </p>
-          </div>
-          <div className={`h-1 bg-linear-to-r ${template.color} transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left`} />
+            <div className={`h-1 bg-linear-to-r ${template.color} transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left`} />
           </div>
         );
       })}
--- frontend/src/components/template_utilities/TemplateSelectorButton.jsx
+++ frontend/src/components/template_utilities/TemplateSelectorButton.jsx
@@ -1,16 +1,33 @@
 import { SquareCheckBig } from "lucide-react"
+import { useState } from "react";
 
 export default function TemplateSelectorButton({ template, onSelectTemplate, onCloseModal }) {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(true);
+  }
+
+  const onLeave = () => {
+    setHover(false);
+  };
+
   const handleSelectTemplate = () => {
     onSelectTemplate(template);
     onCloseModal();
   };
 
   return (
-    <button
-      onClick={handleSelectTemplate}
-      className="p-1 text-portfolio-gradient-1 rounded-full outline-0 hover:p-2 hover:bg-portfolio-text/80 transition">
-      <SquareCheckBig />
-    </button>
+    <div className="flex justify-center items-center">
+      <button
+        onClick={handleSelectTemplate}
+        onMouseEnter={onHover}
+        onMouseLeave={onLeave}
+        // disabled={hover}
+        className="flex items-center text-portfolio-gradient-1 rounded-full outline-0 hover:shadow-2xl transition-all duration-500 hover:-translate-y-2 cursor-pointer">
+        <SquareCheckBig />
+        {hover && <span className="text-portfolio-gradient-1 text-xs ml-1">seleccionar</span>}
+      </button>
+    </div>
   )
 }
